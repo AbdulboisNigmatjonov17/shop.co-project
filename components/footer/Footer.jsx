abfa4effdc3d83656eba0481b177afbdb45cc8ca
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-03-11 16:44:17 +0500

    footer tayyor manimcha

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -2,36 +2,41 @@
 import { FooterData } from '@/helpers/FooterData'
 import { FacebookOutlined, GitHub, Instagram, Twitter } from '@mui/icons-material'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Form from './Form'
 
 export default function Footer() {
   return (
-    <footer className='bg-[#F0F0F0] py-10'>
-      <div className='Container h-[175px] flex flex-wrap justify-between'>
-        <div className='h-full flex flex-col justify-between'>
-          <Image width={165} height={25} src="/SHOP.CO.svg" alt="Logo" />
+    <footer className='bg-[#F0F0F0] pt-40 pb-10 mt-20 text-[#00000099]'>
+      <div className='Container min-h-[175px] flex flex-wrap justify-between relative '>
+        <Form />
+        <div className='w-[250px] h-full flex flex-col justify-between gap-10'>
+          <Link href={'/'}>
+            <Image width={165} height={25} src="/SHOP.CO.svg" alt="Logo" />
+          </Link>
           <p>
             We have clothes that suits your style and which you’re proud to wear. From women to men.
           </p>
           <div className='flex gap-5'>
-            <div className='border border-[#00000033] rounded-full w-[38px] h-[38px] flex justify-center items-center hover:bg-black hover:text-white cursor-pointer'>
+            <div className='border border-[#00000033] rounded-full w-[38px] h-[38px] flex justify-center items-center text-black hover:bg-black hover:text-white cursor-pointer'>
               <Twitter />
             </div>
-            <div className='border border-[#00000033] rounded-full w-[38px] h-[38px] flex justify-center items-center hover:bg-black hover:text-white cursor-pointer'>
+            <div className='border border-[#00000033] rounded-full w-[38px] h-[38px] flex justify-center items-center text-black hover:bg-black hover:text-white cursor-pointer'>
               <FacebookOutlined />
             </div>
-            <div className='border border-[#00000033] rounded-full w-[38px] h-[38px] flex justify-center items-center hover:bg-black hover:text-white cursor-pointer'>
+            <div className='border border-[#00000033] rounded-full w-[38px] h-[38px] flex justify-center items-center text-black hover:bg-black hover:text-white cursor-pointer'>
               <Instagram />
             </div>
-            <div className='border border-[#00000033] rounded-full w-[38px] h-[38px] flex justify-center items-center hover:bg-black hover:text-white cursor-pointer'>
+            <div className='border border-[#00000033] rounded-full w-[38px] h-[38px] flex justify-center items-center text-black hover:bg-black hover:text-white cursor-pointer'>
               <GitHub />
             </div>
           </div>
         </div>
         {
           FooterData.map((item) => (
-            <ul key={item.id} className='h-full flex flex-col justify-between'>
-              <h3>{item.title}</h3>
+            <ul key={item.id} className='min-w-[150px] h-full flex flex-col justify-between gap-5'>
+              <h3 className='font-medium text-[16px] leading-[18px] uppercase text-black mb-1'>{item.title}</h3>
               <li>{item.li1}</li>
               <li>{item.li2}</li>
               <li>{item.li3}</li>
@@ -41,14 +46,14 @@ export default function Footer() {
         }
       </div>
       <hr className='max-w-[1280px] mx-auto my-10 text-[#0000001A]' />
-      <div>
-        <p className='text-[#00000099]'>Shop.co © 2000-2023, All Rights Reserved</p>
+      <div className='Container flex'>
+        <p>Shop.co © 2000-2023, All Rights Reserved</p>
         <div>
+          {/* <img src="" alt="" />
           <img src="" alt="" />
           <img src="" alt="" />
           <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img src="" alt="" /> */}
         </div>
       </div>
     </footer>
--- components/footer/Form.jsx
+++ components/footer/Form.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 export default function Form() {
     return (
-        <section>
-            <h1>STAY UPTO DATE ABOUT OUR LATEST OFFERS</h1>
-            <div>
-                <form className='text-[#00000066]'>
+        <section className='w-full h-[180px] px-16 bg-black text-white absolute -top-[250px] flex items-center justify-between rounded-3xl'>
+            <h1 className='max-w-[550px] text-[40px] font-bold leading-[45px]'>STAY UPTO DATE ABOUT OUR LATEST OFFERS</h1>
+            <div className='max-w-[350px] flex flex-col gap-3.5'>
+                <form className='text-[#00000066] rounded-[62px] bg-white w-full h-[50px] flex items-center gap-3 px-4'>
                     <MailOutline />
-                    <input type="text" placeholder='Enter your email address' />
+                    <input type="text" placeholder='Enter your email address' className='outline-none font-normal text-[16px] w-full pr-2' />
                 </form>
-                <button>Subscribe to Newsletter</button>
+                <button className='w-full h-[52px] rounded-[62px] cursor-pointer bg-white text-black hover:bg-black hover:text-white transition duration-300 ease-in-out'>Subscribe to Newsletter</button>
             </div>
         </section>
     )
